255d86fbbcee4f3bab1c5c23d4b17977e362413b
xubowen <bowen.xu@bangdao-tech.com>
2019-11-21 18:56:19 +0800

    chore: jest repoter

--- jest.config.js
+++ jest.config.js
@@ -16,9 +16,8 @@ module.exports = {
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
   ],
-  testURL: "http://localhost/",
   collectCoverageFrom: ["src/components/**/*.{js,vue}", "!**/node_modules/**"],
-  coverageReporters: ["html", "text-summary"],
+  coverageReporters: ["html", "text-summary", "lcov", "clover"],
   coverageDirectory: "./coverage/",
   collectCoverage: true
 };
