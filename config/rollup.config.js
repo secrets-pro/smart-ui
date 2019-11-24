f633efb191bfdaf85ea757c447bd8079896b9b12
xubowen <bowen.xu@bangdao-tech.com>
2019-11-24 23:45:42 +0800

    feat: base config change

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,18 +1,27 @@
 import vueplugin from "rollup-plugin-vue";
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
+import uglify from "rollup-plugin-uglify-es";
+import svgo from "rollup-plugin-svgo";
 
 let baseconfig = format => {
   return {
     input: "./src/index.js",
     output: {
       name: "sm-ui",
+      globals: {
+        vue: "Vue"
+      },
       extend: true,
       format,
       file: format === "iife" ? "dist/sm-ui.js" : `dist/sm-ui.${format}.js`
     },
     external: ["vue"],
     plugins: [
+      svgo({
+        raw: true
+      }),
+      uglify(),
       resolve(),
       babel({
         exclude: "node_modules/**" // 只编译我们的源代码
